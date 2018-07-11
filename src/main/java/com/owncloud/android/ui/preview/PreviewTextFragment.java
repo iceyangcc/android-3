nextcloud/android

Fix issues found by review

src/com/owncloud/android/ui/preview/PreviewTextFragment.java
@@ -46,8 +46,8 @@ import com.owncloud.android.ui.dialog.ConfirmationDialogFragment;
 import com.owncloud.android.ui.dialog.LoadingDialog;
 import com.owncloud.android.ui.dialog.RemoveFilesDialogFragment;
 import com.owncloud.android.ui.fragment.FileFragment;
-import com.owncloud.android.utils.StringUtils;
 import com.owncloud.android.utils.MimeTypeUtil;
+import com.owncloud.android.utils.StringUtils;
 
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
@@ -101,7 +101,7 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
 
 
         View ret = inflater.inflate(R.layout.text_file_preview, container, false);
-            mTextPreview = (TextView) ret.findViewById(R.id.text_preview);
+        mTextPreview = (TextView) ret.findViewById(R.id.text_preview);
 
         return ret;
     }
@@ -126,13 +126,8 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
             mAccount = args.getParcelable(FileDisplayActivity.EXTRA_ACCOUNT);
         }
 
-        if (args.containsKey(FileDisplayActivity.EXTRA_SEARCH_QUERY)) {
-            mSearchQuery = args.getString(FileDisplayActivity.EXTRA_SEARCH_QUERY);
-        }
-
-        if (args.getBoolean(FileDisplayActivity.EXTRA_SEARCH)) {
-            mSearchOpen = args.getBoolean(FileDisplayActivity.EXTRA_SEARCH, false);
-        }
+        mSearchQuery = args.getString(FileDisplayActivity.EXTRA_SEARCH_QUERY);
+        mSearchOpen = args.getBoolean(FileDisplayActivity.EXTRA_SEARCH, false);
 
         if (savedInstanceState == null) {
             if (file == null) {
@@ -178,42 +173,50 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
 
     @Override
     public boolean onQueryTextSubmit(String query) {
-        if (getActivity().getClass().equals(FileDisplayActivity.class)) {
-            FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
-            fileDisplayActivity.setSearchQuery(query);
-        }
         mHandler.removeCallbacksAndMessages(null);
-        if (query != null && !query.isEmpty()) {
-            String coloredText = StringUtils.searchAndColor(mOriginalText, query);
-            mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
-        } else {
-            mTextPreview.setText(mOriginalText);
+
+        if (mOriginalText != null) {
+            if (getActivity() != null && getActivity() instanceof FileDisplayActivity) {
+                FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
+                fileDisplayActivity.setSearchQuery(query);
+            }
+            if (query != null && !query.isEmpty()) {
+                String coloredText = StringUtils.searchAndColor(mOriginalText, query);
+                mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
+            } else {
+                mTextPreview.setText(mOriginalText);
+            }
+
         }
 
         if (mSearchView != null) {
             mSearchView.clearFocus();
         }
+
         return true;
     }
 
     @Override
     public boolean onQueryTextChange(final String newText) {
-        if (getActivity().getClass().equals(FileDisplayActivity.class)) {
-            FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
-            fileDisplayActivity.setSearchQuery(newText);
-        }
         mHandler.removeCallbacksAndMessages(null);
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (newText != null && !newText.isEmpty()) {
-                    String coloredText = StringUtils.searchAndColor(mOriginalText, newText);
-                    mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
-                } else {
-                    mTextPreview.setText(mOriginalText);
-                }
+
+        if (mOriginalText != null) {
+            if (getActivity().getClass().equals(FileDisplayActivity.class)) {
+                FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
+                fileDisplayActivity.setSearchQuery(newText);
             }
-        }, 500);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (newText != null && !newText.isEmpty()) {
+                        String coloredText = StringUtils.searchAndColor(mOriginalText, newText);
+                        mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
+                    } else {
+                        mTextPreview.setText(mOriginalText);
+                    }
+                }
+            }, 500);
+        }
         return true;
     }
 

iceyangcc/android-3

src/main/java/com/owncloud/android/ui/preview/PreviewTextFragment.java
@@ -54,6 +54,7 @@ import com.owncloud.android.ui.fragment.FileFragment;
 import com.owncloud.android.utils.DisplayUtils;
 import com.owncloud.android.ui.helpers.StringUtils;
 import com.owncloud.android.utils.MimeTypeUtil;
+import com.owncloud.android.utils.MimeTypeUtil;
 
 import org.mozilla.universalchardet.ReaderFactory;
 
@@ -117,6 +118,7 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
 
 
         View ret = inflater.inflate(R.layout.text_file_preview, container, false);
+        mTextPreview = ret.findViewById(R.id.text_preview);
 
         mTextPreview = ret.findViewById(R.id.text_preview);
 
@@ -167,13 +169,8 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
             mAccount = args.getParcelable(FileDisplayActivity.EXTRA_ACCOUNT);
         }
 
-        if (args.containsKey(FileDisplayActivity.EXTRA_SEARCH_QUERY)) {
-            mSearchQuery = args.getString(FileDisplayActivity.EXTRA_SEARCH_QUERY);
-        }
-
-        if (args.getBoolean(FileDisplayActivity.EXTRA_SEARCH)) {
-            mSearchOpen = args.getBoolean(FileDisplayActivity.EXTRA_SEARCH, false);
-        }
+        mSearchQuery = args.getString(FileDisplayActivity.EXTRA_SEARCH_QUERY);
+        mSearchOpen = args.getBoolean(FileDisplayActivity.EXTRA_SEARCH, false);
 
         if (savedInstanceState == null) {
             if (file == null) {
@@ -219,42 +216,50 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
 
     @Override
     public boolean onQueryTextSubmit(String query) {
-        if (getActivity().getClass().equals(FileDisplayActivity.class)) {
-            FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
-            fileDisplayActivity.setSearchQuery(query);
-        }
         mHandler.removeCallbacksAndMessages(null);
-        if (query != null && !query.isEmpty()) {
-            String coloredText = StringUtils.searchAndColor(mOriginalText, query);
-            mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
-        } else {
-            mTextPreview.setText(mOriginalText);
+
+        if (mOriginalText != null) {
+            if (getActivity() != null && getActivity() instanceof FileDisplayActivity) {
+                FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
+                fileDisplayActivity.setSearchQuery(query);
+            }
+            if (query != null && !query.isEmpty()) {
+                String coloredText = StringUtils.searchAndColor(mOriginalText, query);
+                mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
+            } else {
+                mTextPreview.setText(mOriginalText);
+            }
+
         }
 
         if (mSearchView != null) {
             mSearchView.clearFocus();
         }
+
         return true;
     }
 
     @Override
     public boolean onQueryTextChange(final String newText) {
-        if (getActivity().getClass().equals(FileDisplayActivity.class)) {
-            FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
-            fileDisplayActivity.setSearchQuery(newText);
-        }
         mHandler.removeCallbacksAndMessages(null);
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (newText != null && !newText.isEmpty()) {
-                    String coloredText = StringUtils.searchAndColor(mOriginalText, newText);
-                    mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
-                } else {
-                    mTextPreview.setText(mOriginalText);
-                }
+
+        if (mOriginalText != null) {
+            if (getActivity().getClass().equals(FileDisplayActivity.class)) {
+                FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
+                fileDisplayActivity.setSearchQuery(newText);
             }
-        }, 500);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (newText != null && !newText.isEmpty()) {
+                        String coloredText = StringUtils.searchAndColor(mOriginalText, newText);
+                        mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
+                    } else {
+                        mTextPreview.setText(mOriginalText);
+                    }
+                }
+            }, 500);
+        }
         return true;
     }
 